50b3345bf7ccbd9ad8d26d3d8bb5695edac6553b
Karsh <utkarsh.sahay@gmail.com>
2017-07-11 16:52:23 -0400

    Uploaded sidebar-calendar

---
+++ README.md
@@ -0,0 +1,16 @@
+#reddit-sidebar-calendar
+A calendar for embedding into the sidebar of a subreddit
+
+How to install:
+  1. paste the contents of stylesheet.css into your subreddit's stylesheet
+  2. paste the contents of sidebar.md into your subreddit's sidebar where you would like the calendar to be.
+  3. edit the contents of the calendar as you see fit
+
+How to format your calendar:
+ - You may format each day and header with normal markdown
+ - All hover text must be contained within a link text.
+ - You may change which element is used as the container for the hover text in the CSS.
+
+To generate your own calendar (requires Python 3.x):
+ - Run the included sidebar_md_generator.py to generate your own calendar
+ - Coming soon: add your own events from within sidebar_md_generator.py# Reddit Sidebar Calendar
---
+++ sidebar_md_generator.py
@@ -0,0 +1,210 @@
+#!/usr/bin/env python3
+
+from calendar import TextCalendar
+from datetime import date
+
+days_in_month = (31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
+
+class Event:
+    def __init__(self, day, hour, minute, gmt_offset_h, gmt_offset_m):
+        self._day = int(day)
+        self._hour = int(hour)
+        self._minute = int(minute)
+
+        self._tzhr = int(gmt_offset_h)
+        self._tzmin = int(gmt_offset_h)
+
+    def getDay(self):
+        return self._day
+    def getHour(self):
+        return self._hour
+    def getMin(self):
+        return self._minute
+    def getTzLink(self):
+        base_link = "http://localtime.xyz/index.php?"
+        time = self._hour + "%3A" + self._minute
+        tzhr = "offset_h=" + self._tzhr
+        tzmin = "offset_m=" + self._tzmin
+
+        return base_link + time + "&" + tzhr + "&" + tzmin
+
+
+def deep_copy(inList: list) -> list:
+    """Makes a deep copy of a list of lists"""
+    if isinstance(inList, list):
+        return list( map(deep_copy, inList) )
+    return inList
+
+
+def is_leap_year(year: int) -> bool:
+    """Returns whether the given year is a leap year"""
+    if year % 100 == 0:
+        return year % 400 == 0
+    else:
+        return year % 4 == 0
+
+def get_month_ary(year: int, month: int) -> list:
+    """Get the formatted month as a list of lists"""
+    c = TextCalendar(6) # to make week start on Sunday
+
+    month_str = c.formatmonth(year, month)
+    weeks = month_str.split('\n')
+
+    title = weeks[0].strip()
+    weeks = weeks[1:]
+
+    month = [title]
+   
+    for week in weeks:
+        week_list_stripped = week.strip()
+        week_list_split_filtered = list(filter(None, week_list_stripped.split(' ')))
+        month.append(week_list_split_filtered)
+
+    if not month[-1]:
+        month.pop()
+
+    return month[:]
+
+def add_preceding_days(year: int, month: int, month_ary: list) -> list:
+    """Adds preceding days to the first week of the month in-place"""
+    # constant tuple is 0-indexed
+    month = month - 1
+    # check if February
+    if month == 1:
+        # check if leap year
+        prev_days = days_in_month[month] + int(is_leap_year())
+    else:
+        prev_days = days_in_month[month]
+
+    first_week = month_ary[2]
+    i = 0
+    while len(first_week) < 7:
+        first_week.insert(0, str(prev_days - i))
+        i += 1
+    return month_ary
+
+def add_trailing_days(month_ary: list) -> list:
+    """Adds trailing days to the last week of the month in-place."""
+    last_week = month_ary[-1]
+    day = 1
+    while len(last_week) < 7:
+        last_week.append(str(day))
+        day += 1
+
+    return month_ary
+
+def format_markdown(shortdaywk: bool, month_ary: list) -> str:
+    """Formats the given month array as markdown, adding any padding necessary"""
+    # make a copy so that we don't destroy the original
+    month = deep_copy(month_ary)
+
+    title = month[0]
+    header = month[1]
+    month = month[2:]
+
+    # pad the first and last week accordingly
+    first_week = month[0]
+    while len(first_week) < 7:
+        first_week.insert(0, ' ')
+
+    last_week = month[-1]
+    while len(last_week) < 7:
+        last_week.append(' ')
+
+    # convert to Markdown
+    ## add title
+    md_month = ['__', title, '__', '\n\n']
+
+    ## add day of the week as header
+    md_month.append('| ')
+    
+    for day in header:
+        if shortdaywk:
+            md_month.append(day[0])
+        else:
+            md_month.append(day)
+
+        md_month.append(' |')
+        md_month.append(' ')
+
+    md_month[-1] = '\n'
+    md_month.append('|')
+
+    ## add header divider
+    for i in range(7):
+        md_month.append(" --- |")
+
+    md_month.append('\n')
+
+    ## add rest of the days
+    for week in month:
+        if(len(week) != 7):
+            raise IndexError("Week length not 7")
+
+        md_month.append('| ')
+
+        for day in week:
+            md_month.append(day)
+            md_month.append(" |")
+            md_month.append(' ')
+
+        md_month[-1] = '\n'
+
+    return ''.join(md_month)
+
+
+def get_user_events():
+    """Gets a list of events from the user and returns a list of Event objects"""
+    pass
+
+if __name__ == "__main__":
+    today_year = date.today().year
+    today_month = date.today().month
+
+    year = input("What year? [" + str(today_year) + "]: ")
+    month = input("What month? [" + str(today_month) + "]: ")
+
+    if not year:
+        year = today_year
+    else:
+        year = int(year)
+    if not month:
+        month = today_month
+    else:
+        month = int(month)
+
+    trailing = input("Add preceding and trailing days? [Y/n]: ")
+    shortdaywk = input("Single-character days of the week? [Y/n]: ")
+    event_list = get_user_events() # TODO: finish this function
+
+    if not trailing:
+        trailing = True
+    elif trailing == 'y' or trailing == 'Y':
+        trailing = True
+    else:
+        trailing = False
+
+    if not shortdaywk:
+        shortdaywk = True
+    elif shortdaywk == 'y' or shortdaywk == 'Y':
+        shortdaywk = True
+    else:
+        shortdaywk = False
+
+    raw_month = get_month_ary(year, month)
+
+    if trailing:
+        add_preceding_days(year, month, raw_month)
+        add_trailing_days(raw_month)
+
+    formatted_month = format_markdown(shortdaywk, raw_month)
+
+    print()
+    write_to_file = input("Specify filename or leave blank to print to console: ")
+    print()
+    print()
+    if not write_to_file:
+        print(formatted_month)
+    else:
+        with open(write_to_file, 'w') as out_file:
+            out_file.write(formatted_month)
